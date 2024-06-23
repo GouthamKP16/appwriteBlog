4d4b985d3717aafaffe1968ff7dd8df16579cfd2
GouthamKP16 <gouthampreetham567@gmail.com>
2024-06-23 20:29:52 +0530

    changes

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBut from "./header/LogoutBut";
 
-export { Header, Footer };
+export { Header, Footer, Container, Logo, LogoutBut };
